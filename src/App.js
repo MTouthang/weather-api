6785dae7f9c69297b8f1f329e999fe49f6254aef
Mamang touthang <huebart6@gmail.com>
2022-07-11 16:46:07 +0530

    axious and weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+// import axios ---
+import axios from "axios";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState("");
+
+  // calling weather API
+  const fetchWeatherAPI = (local) => {
+    axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${local}&aqi=no`
+      )
+      .then((data) => {
+        console.log(data.data);
+        setWeather(data.data);
+      })
+      .catch((error) => console.log(`ERROR: ${error}`));
+  };
+
+  useEffect(() => {
+    fetchWeatherAPI("Bangalore");
+  }, []);
+
+  const inputLocation = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const searchHandler = () => {
+    fetchWeatherAPI(location);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {weather && (
+        <div>
+          <input
+            type="text"
+            onChange={inputLocation}
+            placeholder="Enter your Location"
+          />
+          <button onClick={searchHandler}> Search</button>
+
+          <h1> {weather.location.name} </h1>
+          <h2> {weather.location.region}</h2>
+          <div className="condition">
+            <h3> {weather.current.condition.text} </h3>
+            <img src={weather.current.condition.icon} alt="weather icon" />
+            <h3> {weather.current.temp_f} F</h3>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
